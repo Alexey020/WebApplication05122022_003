eaac594d47b24b09446e0495d255d5be6683fea5
oleksikoleso@gmail.com <oleksikoleso@gmail.com>
2022-12-05 22:46:20 +0000

    Add project files.

---
+++ WebApplication05122022_003/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication05122022_003.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebApplication05122022_003/Services/GoogleOAuthService.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication05122022_003.Services
+{
+    public class GoogleOAuthService
+    {
+        public static string ClientId { get; }
+        public static string ClientSecret { get; }
+
+        
+    }
+}
