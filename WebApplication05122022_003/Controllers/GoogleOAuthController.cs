898094d89d99a1281fc777045e605646ed94d43e
oleksikoleso@gmail.com <oleksikoleso@gmail.com>
2022-12-05 23:10:56 +0000

    It is still worling, I dont know whyand how

---
+++ WebApplication05122022_003/Controllers/GoogleOAuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using WebApplication05122022_003.Services;
+
+namespace WebApplication05122022_003.Controllers
+{
+    public class GoogleOAuthController : Controller
+    {
+        public static IActionResult RedirectToOAuthServer()
+        {
+            var url = GoogleOAuthService.GenerateRequestUrl();
+            return RedirectToOAuthServer()
+            throw new NotImplementedException();
+        }
+
+    }
+}
