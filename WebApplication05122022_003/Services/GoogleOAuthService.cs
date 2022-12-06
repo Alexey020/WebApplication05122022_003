11446100cfc2e25140ede414a4677d13ef44adc8
oleksikoleso@gmail.com <oleksikoleso@gmail.com>
2022-12-06 00:02:07 +0000

    one more

--- WebApplication05122022_003/Services/GoogleOAuthService.cs
+++ WebApplication05122022_003/Services/GoogleOAuthService.cs
@@ -60,14 +60,8 @@ namespace WebApplication05122022_003.Services
                 {"refresh_token", refreshToken},
 
             };
-            //TODO: helper for creating url ------https://accounts.google.com/o/oauth2/v2/auth?
-            //                                    scope = email % 20profile &
-            //                                    response_type = code &
-            //                                    state = security_token % 3D138r5719ru3e1 % 26url % 3Dhttps % 3A % 2F % 2Foauth2.example.com % 2Ftoken &
-            //                                    redirect_uri = com.example.app % 3A / oauth2redirect &
-            //                                    client_id = client_id
-            //return someHelper.createUrl(TokenServerEndPoint, queryParams);
-            throw new NotImplementedException();
+            var tokenResult = new object();//TODO: create helper for SendPostRequest<token result tyoe>(tokenServerEndPoint, authParams)
+            return tokenResult;
         }
 
     }
